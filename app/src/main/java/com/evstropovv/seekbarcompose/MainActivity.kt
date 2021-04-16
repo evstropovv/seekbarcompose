9d13af9a0a6db8e572562dcc388269a46410344c
vasylyevstropov <vas.evstropov@gmail.com>
2021-04-16 22:29:54 +0300

    add preview

--- app/src/main/java/com/evstropovv/seekbarcompose/MainActivity.kt
+++ app/src/main/java/com/evstropovv/seekbarcompose/MainActivity.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.evstropovv.seekbarcompose.ui.theme.SeekbarComposeTheme
 import kotlinx.coroutines.flow.MutableStateFlow
 
@@ -16,22 +22,44 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SeekbarComposeTheme {
-                Surface(color = MaterialTheme.colors.background) {
-
-                    val position = MutableStateFlow(10L)
-                    val duration = MutableStateFlow(100L)
-
-                    Seekbar(
-                        position = position.collectAsState(),
-                        duration = duration.collectAsState(),
-                        onNewProgress = { },
-                        onDragStart = { },
-                        onDragEnd = {
-                            position.value = it
-                            Toast.makeText(this, "New position: ${position.value}", Toast.LENGTH_SHORT).show()
-                        })
-                }
+                MainScreen()
             }
         }
+    }
+}
+
+@Composable
+fun MainScreen() {
+
+    Column {
+
+        repeat(10) {
+
+            val context = LocalContext.current
+
+            val position = MutableStateFlow(10L)
+            val duration = MutableStateFlow(100L)
+
+            Seekbar(
+                position = position.collectAsState(),
+                duration = duration.collectAsState(),
+                onNewProgress = { },
+                onDragStart = { },
+                onDragEnd = {
+                    position.value = it
+                    Toast.makeText(context, "New position: ${position.value}", Toast.LENGTH_SHORT).show()
+                })
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MainScreenPreview(){
+    SeekbarComposeTheme {
+        MainScreen()
     }
 }
